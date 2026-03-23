2e793d808c575659f6bdb19c4c1a4d2141116116
jparisu <javier.paris.u@gmail.com>
2026-03-23 09:11:09 +0100

    Add more test datasets to risk problem
    Signed-off-by: jparisu <javier.paris.u@gmail.com>

--- datasets/risk/.scripts/generate_datasets.py
+++ datasets/risk/.scripts/generate_datasets.py
@@ -14,11 +14,14 @@ train_df.to_csv("../risk_train_dataset.csv", index=False)
 
 ###############
 # Test dataset
-test_df, _ = generate_dataset(
-    N=500,
-    seed=1,
-    outlier_prob=0.002
-)
+for seed in [1, 24, 42, 20260326]:
 
-# store the dataset as a csv file
-test_df.to_csv("../risk_test_dataset_1.csv", index=False)
+    # generate a test dataset with a different seed and a lower outlier probability
+    test_df, _ = generate_dataset(
+        N=500,
+        seed=seed,
+        outlier_prob=0.002
+    )
+
+    # store the dataset as a csv file
+    test_df.to_csv(f"../risk_test_dataset_{seed}.csv", index=False)
